16c92886fa8f15aa7bea8013806a71a25ee70da3
Jivin Varghese <jivinvarghese007@gmail.com>
2021-07-10 22:45:47 +0530

    updated links

--- script.py
+++ script.py
@@ -1,16 +1,8 @@
 import os
 
 links={
-    "resume":"https://drive.google.com/file/d/1Rjd07Nazmr3-Je-kKLuH0X0ICTjbudtN/view?usp=sharing",
-    "meet":"https://meet.google.com/sac-kfod-ipt",
-    "linkedin":"https://www.linkedin.com/in/haydencordeiro/",
-    "github":"https://github.com/haydencordeiro",
-    "marksheets":"https://drive.google.com/file/d/1A1NP0Xd0sWpnIBZtvLbMHG9M9Esxy-Kn/view?usp=sharing",
-    "adharpanbirth":"https://drive.google.com/file/d/1sz1-alDLg6w6JeCylwSFR_MT5xDyrVEd/view?usp=sharing",
-    "pinelabs":"https://drive.google.com/file/d/1qb4B0LaH26OoS26k_7da5XcydmCOlktM/view?usp=sharing",
-    "nss":"https://drive.google.com/file/d/116l58Woj2uwf8ARL7u7yN0BwKA4dR2D9/view?usp=sharing",
-    "tpc":"https://drive.google.com/file/d/1wg6CSQMSbBcIByR1RKA3XM_P4Yaj6NmX/view?usp=sharing",
-    "yif":"https://drive.google.com/file/d/1qmw0kzT3zaDIO-VbdO80U4PbMT5GvM9m/view?usp=sharing"
+    "resume":"https://drive.google.com/file/d/1PFNn1DiG0Mtp7_XdDuHxldcxFdPjn0eF/view?usp=sharing",
+
 }
 
 
