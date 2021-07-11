dde9e54bd7ca0c05e1acb4267f55d13ae07f2493
Jivin Varghese <jivinvarghese007@gmail.com>
2021-07-11 16:11:04 +0530

    updated links

--- script.py
+++ script.py
@@ -3,7 +3,10 @@ import os
 links={
     "resume":"https://drive.google.com/file/d/1PFNn1DiG0Mtp7_XdDuHxldcxFdPjn0eF/view?usp=sharing",
     "github":"https://github.com/JivinVarghese",
-    "google":"google.com",
+    "marksheets":"https://drive.google.com/file/d/1n4O9l5DsgczCjBG-DpYJ8Qy-IP4MP8M2/view?usp=sharing",
+    "aadhaarpanbirth":"https://drive.google.com/file/d/1Dqgt8rCwyLY3_NcR3rbxrZ76-fiG9pB6/view?usp=sharing",
+    "linkedin":"https://www.linkedin.com/in/jivinvarghese/",
+
 
 }
 
