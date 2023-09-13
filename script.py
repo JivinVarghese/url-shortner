ac1dc0bcdecdd5a4381318e5af3b0d97509bcc5e
Jivin Varghese <jivinvarghese007@gmail.com>
2023-09-12 20:19:05 -0400

    changes to link

--- script.py
+++ script.py
@@ -13,7 +13,7 @@ links={
     "zenscape":"https://play.google.com/store/apps/details?id=com.Teknack.Zenscape&hl=en_IN&gl=US",
     "gre":"https://mega.nz/folder/6bRCVZyK#fJ0wI9IUUP2p3Ypgtsmcdg",
     "ielts":"https://mega.nz/folder/DfIgkDLA#7st_w2l6QKSiyj8m1Zi6JQ",
-    "meet":"https://meet.google.com/sac-kfod-ipt",
+    "meet":"https://meet.google.com/fwp-rrgc-jhj",
     "zoom":"https://us04web.zoom.us/j/7661382898?pwd=Z01xa05kcE9OR2ZVKzFEUDZZWWFXZz09",
     "ruvin":"https://meet.google.com/jbz-agyh-oym",
     "jivin":"https://meet.google.com/kgg-nyxj-gyb",
